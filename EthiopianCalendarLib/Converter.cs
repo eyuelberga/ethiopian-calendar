98d4524ce003bdcf38bab5277c16b38ff37ee2d3
eyuel <eyuel.berga@yahoo.com>
2018-11-20 11:55:37 +0300

    added GregorianToJDN

--- EthiopianCalendarLib/Converter.cs
+++ EthiopianCalendarLib/Converter.cs
@@ -6,5 +6,19 @@ namespace EthiopianCalendar
     /// </summary>
     public static class Converter
     {
+
+        /// <summary>
+        /// Convert Gregorian Date into JDN(Julian Date Number). The algorithm is valid for all (possibly proleptic) Gregorian calendar dates after November 23, −4713.
+        /// </summary>
+        /// <returns>JDN number integer value.</returns>
+        /// <param name="day">Day.</param>
+        /// <param name="month">Month.</param>
+        /// <param name="year">Year.</param>
+        public static int GregorianToJDN(int day = 1, int month = 1, int year = 1)
+        {
+            //TODO error exception if not valid gregorian date
+            return (1461 * (year + 4800 + (month - 14) / 12)) / 4 + (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12 - (3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4 + day - 32075;
+
+        }
     }
 }
