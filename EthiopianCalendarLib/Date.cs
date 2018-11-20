8c0e1a589769c8b1aa675570bff59bba23976f03
eyuel <eyuel.berga@yahoo.com>
2018-11-20 15:32:27 +0300

    added some methods

--- EthiopianCalendarLib/Date.cs
+++ EthiopianCalendarLib/Date.cs
@@ -1,10 +1,45 @@
 ﻿using System;
-namespace EthiopianCalendarLib
+namespace EthiopianCalendar
 {
     public class Date
     {
-        public Date()
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int WeekDayNumber { get; set; }
+        public string Special { get; set; }
+
+        public Date(int day, int month, int year)
         {
+            if (EtDateValidator.IsValid(day, month, year))
+            {
+                Day = day;
+                Month = month;
+                Year = year;
+                WeekDayNumber = Converter.EthiopianWeekDayNumber(day, month, year);
+
+            }
+            else
+            {
+                //TODO exeption error invalid date 
+            }
+
+        }
+        public string Print()
+        {
+            return $"---{WeekDayNumber},{Day},{Month},{Year}";
+        }
+        public string PrintDay()
+        {
+            return $"{Day}";
+        }
+        public string PrintDayName()
+        {
+            return $"{EthiopianLocalized.GetWeekDayName(WeekDayNumber)}";
+        }
+        public string PrintDateH()
+        {
+            return Special == null ? $"{Day}" : $"*";
         }
     }
 }
