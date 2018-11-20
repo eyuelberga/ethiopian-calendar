be46c92e74e4ba1f01def75a21e4c60e5f993d9c
eyuel <eyuel.berga@yahoo.com>
2018-11-20 12:29:43 +0300

    added new constants

--- EthiopianCalendarLib/JDN_CONST.cs
+++ EthiopianCalendarLib/JDN_CONST.cs
@@ -3,6 +3,7 @@ namespace EthiopianCalendar
 {
     public enum JDN_CONST
     {
+        _1460 = 1460,
         _1461 = 1461,
         _4800 = 4800,
         _14 = 14,
@@ -13,6 +14,9 @@ namespace EthiopianCalendar
         _3 = 3,
         _4900 = 4900,
         _100 = 100,
-        _32075 = 32075
+        _32075 = 32075,
+        _365 = 365,
+        _30 = 30
+
     }
 }
