7566f4fde1af3e8253ad9d429fccdaf3558158ea
eyuel <eyuel.berga@yahoo.com>
2018-11-20 11:23:49 +0300

    added enum for date constants

--- EthiopianCalendarLib/DATE_CONFIG.cs
+++ EthiopianCalendarLib/DATE_CONFIG.cs
@@ -1,7 +1,16 @@
 ﻿using System;
-namespace EthiopianCalendarLib
+namespace EthiopianCalendar
 {
+    /// <summary>
+    /// contains constants about the date, month and year in the Ethiopian Calendar
+    /// </summary>
     public enum DATE_CONFIG
     {
+        FIRST_DAY = 1,
+        LAST_DAY = 30,
+        FIRST_MONTH = 1,
+        LAST_MONTH = 13,
+        PAGUME_LAST_DAY = 5,
+        PAGUME_LEAP_YEAR_LAST_DAY = 6
     }
 }
