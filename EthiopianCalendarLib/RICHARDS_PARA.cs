391d492ac2fe830a05f51ee40e956a4cf1a0d1e8
eyuel <eyuel.berga@yahoo.com>
2018-11-20 11:28:10 +0300

    added Richards algo param for G C

--- EthiopianCalendarLib/RICHARDS_PARA.cs
+++ EthiopianCalendarLib/RICHARDS_PARA.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-namespace EthiopianCalendarLib
+namespace EthiopianCalendar
 {
-    public class RICHARDS_PARA
+    /// <summary>
+    /// Algorithm parameters for Gregorian calendar by Richards.
+    /// </summary>
+    public enum RICHARDS_PARA
     {
-        public RICHARDS_PARA()
-        {
-        }
+        y = 4716,
+        v = 3,
+        j = 1401,
+        u = 5,
+        m = 2,
+        s = 153,
+        n = 12,
+        w = 2,
+        r = 4,
+        B = 274277,
+        p = 1461,
+        C = -38
     }
 }
